7f4c880fe857f5c8d2e8c138eeaa302b1c7fa8a5
Edwin Kayang <epkayang@gmail.com>
2025-03-07 02:15:50 -0700

    added base for dispatcher and utils

---
+++ src/dispatcher.c
@@ -0,0 +1,17 @@
+#include "dispatcher.h"
+
+uint64_t init(uint64_t *TRAPFRAME){
+
+}
+
+uint64_t stub(uint64_t* TRAPFRAME, uint64_t taken, int shouldInstrument, uint64_t lastInstAddress){
+
+}
+
+uint64_t instrument(uint64_t* TRAPFRAME, uint64_t taken){
+
+}
+
+void exit_binary(uint64_t *TRAPFRAME){
+
+}
--- src/dispatcher.h
+++ src/dispatcher.h
@@ -1,4 +1,36 @@
 #ifndef DISPATCHER_H
 #define DISPATCHER_H
 
+#include <stdint.h>
+#include "elf_reader.h"
+
+extern ElfReader elfReaderPtr;
+
+/**
+ * Initializes registers and call PRE routines before execution starts
+ * @param TRAPFRAME stack pointer value for trapframe
+*/
+extern uint64_t init(uint64_t *TRAPFRAME);
+
+/**
+ * Runs instrumentation routines and switches control to the next block
+ * @param TRAPFRAME stack pointer value for trapframe
+ * @param taken informs whether or not branch was taken
+ * @param shouldInstrument informs whether or not to run instrumentation routines
+*/
+extern uint64_t stub(uint64_t* TRAPFRAME, uint64_t taken, int shouldInstrument, uint64_t lastInstAddress);
+
+/**
+ * Calls PRE routines for instruction types
+ * @param TRAPFRAME stack pointer value for trapframe
+ * @param taken informs whether or not branch was taken
+*/
+extern uint64_t instrument(uint64_t* TRAPFRAME, uint64_t taken);
+
+/**
+ * Calls exit function and exits from RAIL
+ * @param TRAPFRAME stack pointer value for trapframe
+*/
+extern void exit_binary(uint64_t *TRAPFRAME);
+
 #endif
